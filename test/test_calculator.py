e24325f3a262032e965f7da100a3538d6fbb5276
HaroldMontanoHurtado <harold.montano@correounivalle.edu.co>
2024-03-18 06:46:40 -0500

    mover archivos

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,10 +1,7 @@
-from unittest import TestCase, main
+from unittest import TestCase
 from calculator import mul
 
 class TestCalculator(TestCase):
     
     def test_mul(self):
         self.assertEqual(mul(2, 2), 4)
-
-#if __name__ == '__main__':
-#    main()
